fb1e8907244b7bac292afaec56234b7f9af464f2
Divine <divine2122@comcast.net>
2022-01-19 11:49:18 +0100

    redirect is direct link instead of call

--- src/services/datastore.js
+++ src/services/datastore.js
@@ -2,11 +2,13 @@ const fetch = require('node-fetch');
 
 
 export async function authFunction() {
-
     const params = new URLSearchParams(window.location.search);
     const requestToken = params.get('code');
 
+    console.log('authfunc ran',requestToken)
+
         const response = await fetch(`http://localhost:3035/home?code=${requestToken}&state=SOME_STATE_VALUE`, {
+
         method: "GET",
         headers: {
             Accept: "application/json",
@@ -18,6 +20,26 @@ export async function authFunction() {
         return output;
 }
 
+
+export async function preAuthFunction() {
+  // const params = new URLSearchParams(window.location.search);
+  // const requestToken = params.get('code');
+
+  console.log('preauthfunc ran')
+
+      const response = await fetch(`http://localhost:3035/auth`, {
+
+      method: "GET",
+      headers: {
+
+          // Accept: "application/json",
+          // "Content-type": "application/json",
+      },
+      });
+
+      let output = await response
+      return output;
+}
 // export async function translateFunction() {
 
 //   const params = new URLSearchParams(window.location.search);
