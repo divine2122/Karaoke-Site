f67cc28e38893e7599d448ac3b3bd47f97ff6022
Divine <divine2122@comcast.net>
2022-01-31 19:40:30 +0100

    form added for user query search now hits backend for song lyrics

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,9 +10,23 @@ export class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-  
+          searchQuery:null
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
       }
+
+
+
+  handleChange(event) {
+    this.setState({searchQuery: event.target.value});
+  }
+
+  handleSubmit(event) {
+    //alert('A name was submitted: ' + this.state.searchQuery);
+    event.preventDefault();
+  }
 
 render(){
 
@@ -22,9 +36,14 @@ render(){
         <p>Welcome, youre just in time for karaoke night! Pick your player</p>
       </div>
 
-      <button onClick={event =>  window.location.href=`https://api.genius.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=me&state=SOME_STATE_VALUE&response_type=code`}>
-        Authenticate Rapgenius
-      </button>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input type="text" value={this.state.searchQuery} onChange={this.handleChange} />
+        </label>
+        <br></br>
+
+        <input type="submit" value="Authenticate Rapgenius" onClick={event =>  window.location.href=`https://api.genius.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=me&state=${this.state.searchQuery}&response_type=code`}/>
+      </form>  
 
       </div>
   );
